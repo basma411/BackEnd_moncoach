7cfc81de7e1bfadd6c842c21f5775a4da512fba7
basmasabkhi <sabkhi.basma89@gmail.com>
2024-06-12 04:57:15 +0200

    version45

--- controllers/Contact.js
+++ controllers/Contact.js
@@ -1,20 +1,45 @@
 const Contacts = require('../models/ContactSchema');
+const nodemailer = require('nodemailer');
+
 const AddContact = async (req, res) => {
     try {
         // Récupérer les données du corps de la requête
         const { nom_prenom, tel, email, mssg } = req.body;
 
-
         // Sauvegarder le nouveau contact dans la base de données
-        const savedContact =await Contacts.create({
-            nom_prenom, tel,  email,mssg
+        const savedContact = await Contacts.create({
+            nom_prenom, tel, email, mssg
         });
 
-        res.status(201).json({ success: true, message: "Contact ajouté avec succès", contact: savedContact });
+        // Configuration de Nodemailer
+        const transporter = nodemailer.createTransport({
+            service: 'gmail', // Utilisez le service de votre choix
+            auth: {
+                user: 'sabkhibasma1@gmail.com', // Votre adresse email
+                pass: 'tkcb mzjy czxa slmb'   // Votre mot de passe email
+            }
+        });
+
+        // Options de l'email
+        const mailOptions = {
+            from: email, // L'adresse email de l'expéditeur
+            to: "contact@moncoach.tn",
+            subject: `New Contact from ${nom_prenom}`,
+            text: `Name: ${nom_prenom}\nPhone: ${tel}\nEmail: ${email}\nMessage: ${mssg}`
+        };
+
+        // Envoyer l'email
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return res.status(500).json({ success: false, message: "Erreur lors de l'envoi de l'email", error: error.toString() });
+            }
+            res.status(201).json({ success: true, message: "Contact ajouté avec succès et email envoyé", contact: savedContact });
+        });
     } catch (err) {
         res.status(500).json({ success: false, message: "Erreur lors de l'ajout du contact", error: err.message });
     }
-}
+};
+
 const GetContact=async(rq,res)=>{
     try {
         const Contact=await Contacts.find()
